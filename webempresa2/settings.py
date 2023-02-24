dc2c9bcdbb699a7e4c505f34be474460dbf3ef21
darko262 <84549286+darko262@users.noreply.github.com>
2023-02-24 12:48:41 -0300

    Update settings.py

--- webempresa2/settings.py
+++ webempresa2/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dq2bjvrg5sazbi$4r=kj%hxln$l0zi8kb6a$4vt!p=o6!u$tak'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
