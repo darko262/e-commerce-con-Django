17d0668b0cdccec9110c9d034916c3d46a655b73
darko262 <84549286+darko262@users.noreply.github.com>
2023-02-27 12:15:55 -0300

    Update views.py

--- productos/views.py
+++ productos/views.py
@@ -32,7 +32,7 @@ def productos(request ,category_slug=None):
         'productos':paged_product,#'filtrado' : filtro 
         'product_count':product_count,
     }
-    return render(request, "productos/productos.html ",contex)
+    return render(request, 'productos/productos.html ',contex)
 
 def productos_detail(request, category_slug, product_slug):
     try:
@@ -64,4 +64,4 @@ def search(request):
         'productos':products,
         'product_count':product_count,
     }
-    return render(request, "productos/productos.html",contex)
+    return render(request, 'productos/productos.html',contex)
